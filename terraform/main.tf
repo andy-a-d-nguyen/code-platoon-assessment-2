5a525e9c6a0d8023f438c61e9f3fddb996c85d37
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2025-01-25 03:08:45 -0800

    WIP - Provision managed MySQL with Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,6 +49,27 @@ resource "aws_instance" "ubuntu_server" {
   }
 }
 
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "andy"
+  password             = "andyandy"
+  db_name              = "todo_db"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+
+  tags = {
+    Name = "Andy MySQL"
+  }
+}
+
 output "instance_ip" {
   value = aws_instance.ubuntu_server.public_ip
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.default.endpoint
 }
